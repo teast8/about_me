9cb100679c9f1aea8878d9fe25aa571f6c2a31b7
bronwyndet <wayward@waywardcoffee.com>
2016-07-21 14:01:16 -0700

    Question 2 function added, debugged Question 6 and now working.

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ var userName = prompt('Welcome everyone, you just entered Ty\s dope ass page, Wh
 
 alert('Hi, ' + userName + ', welcome! Let\'s see if you can answer some questions about Ty! Please answer yes/no or y/n.');
 
-function questionOne () {
+// Question 1 wrapped in a function by Ty and Bronwyn
+function questionOne() {
   var answer1 = prompt('Did Ty Play Soccer in College?');
   if (answer1.toLowerCase() === 'no' || answer1.toLowerCase() === 'n') {
     alert('You got it right, ' + userName + '! Ty Did not play soccer in college.');
@@ -17,14 +18,17 @@ function questionOne () {
 }
 questionOne();
 
-var answer2 = prompt('Does Ty have 2 siblings?');
-
-if (answer2.toLowerCase() === 'yes' || answer2.toLowerCase() === 'y') {
-  alert('You got it right, ' + userName + '! Ty does have 2 siblings.');
-  correctAnswers += 1;
-} else {
-  alert('Wrong, ' + userName + ', try again next time. Ty has 2 siblings.');
+//Question 2 wrapped in a function by Ty and Bronwyn
+function questionTwo() {
+  var answer2 = prompt('Does Ty have 2 siblings?');
+  if (answer2.toLowerCase() === 'yes' || answer2.toLowerCase() === 'y') {
+    alert('You got it right, ' + userName + '! Ty does have 2 siblings.');
+    correctAnswers += 1;
+  } else {
+    alert('Wrong, ' + userName + ', try again next time. Ty has 2 siblings.');
+  }
 }
+questionTwo();
 
 var answer3 = prompt('What is Ty favorite color?');
 
@@ -56,7 +60,7 @@ if (answer5.toLowerCase() === 'no' || answer5.toLowerCase() === 'n') {
 var age = 27;
 
 for (var i = 0; i < 4; i++) {
-  var ageAnswer = parsInt(prompt('How old is Ty?'));
+  var ageAnswer = parseInt(prompt('How old is Ty?'));
   console.log('i is equal to', i);
   if ((i === 3) && (parseInt(ageAnswer) !== age)) {
     alert ('You ran out of Chances, Ty\'s correct age is 27');
